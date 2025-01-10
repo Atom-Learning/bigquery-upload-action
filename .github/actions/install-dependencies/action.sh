ebcec905f7784bee812269c560cd67fa311d8c7e
Calum Daines <calum.daines@atomlearning.co.uk>
2025-01-10 15:11:07 +0000

    bump deprecated artifact version to v4 (#3)
    * bump deprecated artifact version to v4
    
    * bump deprecated artifact version to v4
    
    * fix matrix python version & retention time
    
    * fix matrix python versions
    
    * pin ubuntu version to 22.04
    
    * pin ubuntu version to 20.04
    
    * disable pip caching installs
    
    * remove 3.6 and 3.7 python versions
    
    * use consistent version 3.11 in docker base image and pipeline
    
    * fix(security): bump to 3.11.11
    
    * update changelog

--- .github/actions/install-dependencies/action.sh
+++ .github/actions/install-dependencies/action.sh
@@ -5,10 +5,10 @@ python -m pip install --upgrade pip
 
 if [[ "${INSTALL_REQUIREMENTS}" == "true"  ]]; then
   echo "Installing code requirements"
-  pip install -r plugin_scripts/requirements.lock
+  pip install --no-cache-dir -r plugin_scripts/requirements.lock
 fi
 
 if [[ "${INSTALL_TEST_REQUIREMENTS}" == "true"  ]]; then
   echo "Installing test requirements"
-  pip install -r requirements-test.txt
+  pip install --no-cache-dir -r requirements-test.txt
 fi
